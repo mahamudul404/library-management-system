6eec880e278bd7b672147ed30dac85564ae91a1f
Your New Name <mahamudul408432@gmail.com>
2024-11-02 00:07:37 +0600

    nav add

--- resources/views/books/borrowed.blade.php
+++ resources/views/books/borrowed.blade.php
@@ -1,1 +1,29 @@
-<h1>test</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Borrowed Books</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-900 text-gray-200">
+
+    {{-- Navbar --}}
+    <nav class="bg-gray-800 p-4 shadow-lg">
+        <div class="container mx-auto flex justify-between items-center">
+            <a href="/" class="text-2xl font-bold text-indigo-500">Library Management</a>
+            <div class="space-x-4">
+                <a href="/" class="text-gray-300 hover:text-indigo-400">Home</a>
+                
+            </div>
+        </div>
+    </nav>
+
+  
+           
+
+</body>
+
+</html>
